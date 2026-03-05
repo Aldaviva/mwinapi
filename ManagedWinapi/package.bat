0b10c7d5d7d3809732fe430b42e19216c654c7bf
Ben Hutchison <ben@aldaviva.com>
2026-03-05 07:08:55 -0800

    Added package icon (owner's avatar)

--- ManagedWinapi/package.bat
+++ ManagedWinapi/package.bat
@@ -1,7 +1,7 @@
 @echo off
 
-dotnet clean -c ReleaseUnsigned ManagedWinapi-2022.csproj
-dotnet clean -c ReleaseSigned   ManagedWinapi-2022.csproj
+dotnet clean -c ReleaseUnsigned ManagedWinapi-2026.csproj
+dotnet clean -c ReleaseSigned   ManagedWinapi-2026.csproj
 
-dotnet pack -c ReleaseUnsigned --output .\bin\Release ManagedWinapi-2022.csproj
-dotnet pack -c ReleaseSigned   --output .\bin\ReleaseSigned ManagedWinapi-2022.csproj
+dotnet pack -c ReleaseUnsigned --output .\bin\ReleaseUnsigned ManagedWinapi-2026.csproj
+dotnet pack -c ReleaseSigned   --output .\bin\ReleaseSigned ManagedWinapi-2026.csproj
